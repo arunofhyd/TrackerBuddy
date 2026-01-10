afb6b38cda4aad5d3b4c02ee1522d86bb7408c0c
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-01-10 20:49:23 +0530

    Remove background class from search container
    Removed background class from admin search container.

--- app.js
+++ app.js
@@ -4360,7 +4360,7 @@ function renderAdminUserList(users, searchQuery = '') {
     if (!searchContainer) {
         searchContainer = document.createElement('div');
         searchContainer.id = 'admin-search-container';
-        searchContainer.className = 'mb-4 sticky top-0 z-10 pt-1 pb-2 px-1 -mx-1 bg-white dark:bg-gray-800'; // Added bg for sticky
+        searchContainer.className = 'mb-4 sticky top-0 z-10 pt-1 pb-2 px-1 -mx-1'; // Added bg for sticky
         searchContainer.innerHTML = `
             <div class="relative mx-2">
                 <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none" style="padding-left: 1rem;">
